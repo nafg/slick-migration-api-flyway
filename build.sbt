7b884718acbc236441f432b754222b746b8bd50e
tzvi <tzvigons@google.com>
2015-04-22 19:27:02 -0400

    fix code and change class names

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+
 libraryDependencies += "io.github.nafg" %% "slick-migration-api" % "0.1-SNAPSHOT"
 
 libraryDependencies += "org.flywaydb" % "flyway-core" % "3.0"
--- src/main/scala/flygration.scala
+++ src/main/scala/flygration.scala
@@ -19,48 +19,52 @@ package flygration
  */
 
 import java.sql.Connection
-//import java.sql.PreparedStatement
 import org.flywaydb.core.api._
 import org.flywaydb.core.api.resolver._
-//import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
-//import org.flywaydb.core.Flyway
-import scala.slick.migration.api.Migration
+import scala.slick.migration.api._
 import scala.slick.jdbc.UnmanagedSession
 import scala.collection.JavaConverters._
 /* 
  * Resolves available migrations
- * This one may work better but Collection is invariant what do I do?
- * Why does it compile now that I changed the original contract?
  * */
-class MResolver (fs:FWMigration*) extends MigrationResolver {
+class SMAMigrationResolver (ms:FWMigration*) extends MigrationResolver {
   /*
-   * Collection<ResolvedMigration> 	resolveMigrations()
    * Resolves the available migrations.
-   * ensure this returns a collection. Perhaps each migration tuple may require its own collection
    */
-  
   def resolveMigrations: java.util.Collection[ResolvedMigration] = {
-    val rms = for (f <- fs) yield new ResolvedM(f.version, f.toString, f.deltas): ResolvedMigration
+    val rms = for (m <- ms) yield new SMAResolvedMigration(m): ResolvedMigration
     rms.asJavaCollection
   }
 }
 
 /* Migration resolved through a MigrationResolver. Can be applied against a database.*/
-class ResolvedM(version: String, desc: String, deltas: Seq[Migration]) extends ResolvedMigration {
+class SMAResolvedMigration(m: FWMigration) extends ResolvedMigration {
   def getChecksum = null
-  def getDescription: String =  desc
-  //do I need a separate executer for each migration
-  def getExecutor: MigrationExecutor = new MExecuter(deltas: _*) 
+  def getDescription: String =  m.desc
+  def getExecutor: MigrationExecutor = new SMAMigrationExecutor(m.deltas: _*)
   def getPhysicalLocation: String = "Custom code"
-  def getScript: String = s"<< slick-migration-api >>"
+  def getScript: String = m.deltas.map{
+    case m: TableMigration[_] => m.toString
+    case m: SqlMigration   => s"SqlMigration{${m.sql.mkString(";")};}"
+    case _                 => "<< slick-migration-api >>"
+  }.mkString("\n")
   def getType: MigrationType = MigrationType.CUSTOM
-  def getVersion: MigrationVersion = MigrationVersion fromVersion version
+  def getVersion: MigrationVersion = MigrationVersion fromVersion m.version
 }
 /* Executes a migration */
-class MExecuter(delta: Migration*) extends MigrationExecutor {
+class SMAMigrationExecutor(delta: Migration*) extends MigrationExecutor {
   def executeInTransaction = true
   def execute(c: Connection) = delta foreach (_.apply()(new UnmanagedSession(c)))
 }
 
+case class FWMigration(version: String, desc: String, deltas: Seq[Migration])
 
-case class FWMigration(version: String, deltas: Seq[Migration])
+object FWMigration {
+  def apply(version: String, deltas: Seq[Migration]): FWMigration = {
+    val desc = deltas.map{
+      case m: TableMigration[_] => s"TableMigration(${m.tableInfo.tableName})"
+      case other => other.getClass.getName
+    }.mkString("\n")
+    FWMigration(version, desc, deltas)
+  }
+}
