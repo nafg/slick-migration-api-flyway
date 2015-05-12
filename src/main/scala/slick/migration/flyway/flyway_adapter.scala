81b672468207a6a9897a91ba4a3023b779292ab5
Daniel Beskin <daniel.beskin@gmail.com>
2015-05-13 01:31:21 +0300

    Removing the implicit class and using a `Resolver` object instead

--- src/main/scala/slick/migration/flyway/flyway_adapter.scala
+++ src/main/scala/slick/migration/flyway/flyway_adapter.scala
@@ -1,5 +1,31 @@
+/** The `slick.migration.flyway` package is an adapter between the `Flyway` database migration tool,
+ *  and the `slick-migration-api` library.
+ *
+ *  One can aggregate [[scala.slick.migration.api.Migration]]s into [[VersionedMigration]]
+ *  objects and then pass them to `Flyway` as follows
+ *  {{{
+ *  val m1 = TableMigration(testTable)
+ *    .create
+ *    .addColumns(_.col1, _.col2)
+ *
+ *  val m2 = SqlMigration("insert into testtable (col1, col2) values (1, 2)")
+ *
+ *  val migration = VersionedMigration("1", m1, m2)
+ *
+ *  val flyway = new Flyway()
+ *  flyway.setDataSource(db, "", "")
+ *  flyway.setLocations()
+ *
+ *  flyway.setResolvers(Resolver(migration))
+ *
+ *  flyway.migrate()
+ *  }}}
+ *
+ *  (Note that we must use `setLocations()` in order to to avoid searching
+ *  for migrations in the default locations)
+ */
 package scala.slick
-package migration
+package migration.flyway
 
 import java.sql.Connection
 import org.flywaydb.core.api._
@@ -9,89 +35,62 @@ import scala.slick.jdbc.UnmanagedSession
 import scala.collection.JavaConverters._
 import org.flywaydb.core.Flyway
 
-/** The `slick.migration.flyway` package is an adapter between the `Flyway` database migration tool,
- *  and the `slick-migration-api` library.
- *  
- *  The [[SlickMigrationFlywayAdapter]] class adds an implicit method that allows
- *  adding [[scala.slick.migration.api.Migration]]s to a `Flyway` object.
- *  An example session might be:
- *  {{{
- *  val m1 = TableMigration(testTable)
- *    .create
- *    .addColumns(_.col1, _.col2)
- *
- *  val m2 = SqlMigration("insert into testtable (col1, col2) values (1, 2)")
- *  
- *  val migration = VersionedMigration("1", m1, m2)
- *  
- *  val flyway = new Flyway()
- *  flyway.setDataSource(db, "", "")
- *  flyway.setLocations()
- *
- *  flyway.addSlickMigrations(migration1, migration2)
- *
- *  flyway.migrate()
- *  }}}
- * 
- * (Note that we must use `setLocations()` in order to to avoid searching 
- * for migrations in the default locations)
- */
-package object flyway {
-  /** Provides a method to append [[VersionedMigration]]s to a [[Flyway]] object. */
-  implicit class SlickMigrationFlywayAdapter(flyway: Flyway) {
-    /** Appends the given [[VersionedMigration]]s to the wrapped [[Flyway]] object.
-     *  This invokes the [[Flyway#setResolvers]] method in order to add the migrations
-     *  to any existing [[org.flywaydb.core.api.resolver.MigrationResolver]]s.
-     *  In case other resolvers are needed, first set them, and only then invoke this
-     *  method.
-     */
-    def addSlickMigrations(migrations: VersionedMigration*): Unit = {
-      val resolver = new MigrationResolver {
-         def resolveMigrations = migrations.map(versionedMigrationToResolvedMigration).asJavaCollection
-      }
-      val curResolvers = flyway.getResolvers
-      val allResolvers = curResolvers :+ resolver
-      
-      flyway.setResolvers(allResolvers: _*)
-    }
+/** Wraps one or more [[scala.slick.migration.api.Migration]] objects with a version string. */
+case class VersionedMigration(version: String, migrations: Migration*) extends ResolvedMigration {
+  def getChecksum = null
 
-    private def versionedMigrationToResolvedMigration(vm: VersionedMigration): ResolvedMigration =
-      new ResolvedMigration {
-        def getChecksum = null
-
-        def getDescription: String = {
-          vm.migrations.map {
-            case m: TableMigration[_] => s"TableMigration(${m.tableInfo.tableName})"
-            case other => other.getClass.getName
-          }.mkString("\n")
-        }
-
-        def getExecutor: MigrationExecutor = new MigrationExecutor {
-          def executeInTransaction = true
-          def execute(c: Connection) = vm.migrations foreach (_.apply()(new UnmanagedSession(c)))
-        }
-
-        def getPhysicalLocation: String = "Custom code"
-
-        def getScript: String = vm.migrations.map {
-          case m: TableMigration[_] => m.toString
-          case m: SqlMigration => s"SqlMigration{${m.sql.mkString(";")};}"
-          case _ => "<< slick-migration-api >>"
-        }.mkString("\n")
-        
-        def getType: MigrationType = MigrationType.CUSTOM
-        
-        def getVersion: MigrationVersion = MigrationVersion fromVersion vm.version
-      }
+  def getDescription: String = {
+    migrations.map {
+      case m: TableMigration[_] => s"TableMigration(${m.tableInfo.tableName})"
+      case other => other.getClass.getName
+    }.mkString("\n")
   }
+
+  def getExecutor: MigrationExecutor = new MigrationExecutor {
+    def executeInTransaction = true
+    def execute(c: Connection) = migrations foreach (_.apply()(new UnmanagedSession(c)))
+  }
+
+  def getPhysicalLocation: String = "Custom code"
+
+  def getScript: String = migrations.map {
+    case m: TableMigration[_] => m.toString
+    case m: SqlMigration => s"SqlMigration{${m.sql.mkString(";")};}"
+    case _ => "<< slick-migration-api >>"
+  }.mkString("\n")
+
+  def getType: MigrationType = MigrationType.CUSTOM
+
+  def getVersion: MigrationVersion = MigrationVersion fromVersion version
 }
 
-package flyway {
-  /** Wraps one or more [[scala.slick.migration.api.Migration]] objects with a version string. */
-  case class VersionedMigration(version: String, migrations: Migration*)
+object VersionedMigration {
+  def apply(version: Int, migrations: Migration*): VersionedMigration =
+    VersionedMigration(version.toString, migrations: _*)
+}
 
-  object VersionedMigration {
-    def apply(version: Int, migrations: Migration*): VersionedMigration =
-      VersionedMigration(version.toString, migrations: _*)
+object Resolver {
+  /** Converts a list of [[ResolvedMigration]]s into a [[MigrationResolver]].
+   *  This should be used in conjunction with the [[Flyway#setResolvers]] method.
+   */
+  def apply(migrations: ResolvedMigration*) = new MigrationResolver {
+    def resolveMigrations = migrations.asJavaCollection
   }
+  
+// TODO can't have these methods due to erasure, consider an implicit conversion instead
+//  /** Converts a list of pairs of (version, migrations) into a [[MigrationResolver]].
+//   *  This should be used in conjunction with the [[Flyway#setResolvers]] method.
+//   */
+//  def apply(migrations: (String, Seq[Migration])*): MigrationResolver = {
+//    val versioned = migrations.map { case (v, ms) => VersionedMigration(v, ms: _*) }
+//    apply(versioned: _*)
+//  }
+//
+//  /** Converts a list of pairs of (version, migrations) into a [[MigrationResolver]].
+//   *  This should be used in conjunction with the [[Flyway#setResolvers]] method.
+//   */
+//  def apply(migrations: (Int, Seq[Migration])*): MigrationResolver = {
+//    val versioned = migrations.map { case (v, ms) => VersionedMigration(v, ms: _*) }
+//    apply(versioned: _*)
+//  }
 }
