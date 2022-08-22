e268e49e51e90a67e2f68d09c26371b607743065
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2022-08-22 00:17:01 -0400

    Migrate code

--- src/main/scala/slick/migration/api/flyway/ExplicitMigrationResolver.scala
+++ src/main/scala/slick/migration/api/flyway/ExplicitMigrationResolver.scala
@@ -1,8 +1,9 @@
 package slick.migration.api.flyway
 
 import scala.collection.JavaConverters._
+import org.flywaydb.core.api.resolver.MigrationResolver.Context
 
-import org.flywaydb.core.api.resolver.{Context, MigrationResolver, ResolvedMigration}
+import org.flywaydb.core.api.resolver.{MigrationResolver, ResolvedMigration}
 
 
 case class ExplicitMigrationResolver(migrations: ResolvedMigration*) extends MigrationResolver {
