48960aadd2d4484e2da5d88f63d81279129de56b
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2022-12-17 23:14:30 -0500

    FlywayAdapterSpecs: instead of passing in MigrationResolvers instances, define them so we can pass in the Class

--- src/test/scala/slick/migration/api/flyway/FlywayAdapterSpecs.scala
+++ src/test/scala/slick/migration/api/flyway/FlywayAdapterSpecs.scala
@@ -12,7 +12,7 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 
-class FlywayAdapterSpecs extends AnyFreeSpec with Matchers with ScalaFutures with IntegrationPatience {
+object FlywayAdapterSpecs {
   implicit def infoProvider: MigrationInfo.Provider[Migration] = MigrationInfo.Provider.strict
 
   // note, not using capital letters in the table/column names breaks the test
@@ -40,35 +40,62 @@ class FlywayAdapterSpecs extends AnyFreeSpec with Matchers with ScalaFutures wit
     def tableContents = database.run(testTable.result)
   }
 
+  object slickMigrate {
+    val db = DBWrap("slick_migrate")
+
+    val m1 = TableMigration(testTable)
+      .create
+      .addColumns(_.col1, _.col2)
+
+    val m2 = SqlMigration("insert into testtable (col1, col2) values (1, 2)")
+
+    val migration1 = VersionedMigration("1", m1 & m2)
+
+    val m3 = TableMigration(testTable)
+      .addColumns(_.col3)
+
+    val m4 = SqlMigration("insert into testtable (col1, col2, col3) values (10, 20, 30)")
+
+    val migration2 = VersionedMigration("2", m3 & m4)
+
+    class Resolver extends ExplicitMigrationResolver(migration1, migration2)
+  }
+
+  object slickStepMigrate {
+    val db = DBWrap("slick_step_migrate")
+
+    val threeColumns = TableMigration(testTable)
+      .create
+      .addColumns(_.col1, _.col2, _.col3)
+    val dataLine1 = SqlMigration("insert into testtable (col1, col2) values (1, 2)")
+
+    val addThreeColumnsAnd1RowOfData = VersionedMigration("1", threeColumns & dataLine1)
+
+    val dataLine2 = SqlMigration("insert into testtable (col1, col2, col3) values (10, 20, 30)")
+
+    val addAnotherRow = VersionedMigration("2", dataLine2)
+
+    class Resolver1 extends ExplicitMigrationResolver(addThreeColumnsAnd1RowOfData)
+    class Resolver2 extends ExplicitMigrationResolver(addThreeColumnsAnd1RowOfData, addAnotherRow)
+  }
+}
+class FlywayAdapterSpecs extends AnyFreeSpec with Matchers with ScalaFutures with IntegrationPatience {
 
   import ExecutionContext.Implicits.global
+
+  import FlywayAdapterSpecs._
 
 
   "The flyway/slick migrations adapter" - {
     "apply slick migrations via a flyway object" in {
-      val db = DBWrap("slick_migrate")
+      import slickMigrate._
       import db._
-
-      val m1 = TableMigration(testTable)
-        .create
-        .addColumns(_.col1, _.col2)
-
-      val m2 = SqlMigration("insert into testtable (col1, col2) values (1, 2)")
-
-      val migration1 = VersionedMigration("1", m1 & m2)
-
-      val m3 = TableMigration(testTable)
-        .addColumns(_.col3)
-
-      val m4 = SqlMigration("insert into testtable (col1, col2, col3) values (10, 20, 30)")
-
-      val migration2 = VersionedMigration("2", m3 & m4)
 
       val flyway =
         Flyway.configure()
           .dataSource(dbAddress, "", "")
           .locations(Seq.empty[String]: _*)
-          .resolvers(ExplicitMigrationResolver(migration1, migration2))
+          .resolvers(classOf[Resolver].getName)
           .load()
 
       tableExists.futureValue shouldBe false
@@ -80,20 +107,8 @@ class FlywayAdapterSpecs extends AnyFreeSpec with Matchers with ScalaFutures wit
     }
 
     "progressively apply slick migrations via flyway objects" in {
-      val db = DBWrap("slick_step_migrate")
+      import slickStepMigrate._
       import db._
-
-      val threeColumns = TableMigration(testTable)
-        .create
-        .addColumns(_.col1, _.col2, _.col3)
-
-      val dataLine1 = SqlMigration("insert into testtable (col1, col2) values (1, 2)")
-
-      val addThreeColumnsAnd1RowOfData = VersionedMigration("1", threeColumns & dataLine1)
-
-      val dataLine2 = SqlMigration("insert into testtable (col1, col2, col3) values (10, 20, 30)")
-
-      val addnotherRow = VersionedMigration("2", dataLine2)
 
       tableExists.futureValue shouldBe false
 
@@ -101,7 +116,7 @@ class FlywayAdapterSpecs extends AnyFreeSpec with Matchers with ScalaFutures wit
         Flyway.configure()
           .dataSource(dbAddress, "", "")
           .locations(Seq.empty[String]: _*)
-          .resolvers(ExplicitMigrationResolver(addThreeColumnsAnd1RowOfData))
+          .resolvers(classOf[Resolver1].getName)
           .load()
 
       flyway1.migrate()
@@ -113,7 +128,7 @@ class FlywayAdapterSpecs extends AnyFreeSpec with Matchers with ScalaFutures wit
         Flyway.configure()
           .dataSource(dbAddress, "", "")
           .locations(Seq.empty[String]: _*)
-          .resolvers(ExplicitMigrationResolver(addThreeColumnsAnd1RowOfData, addnotherRow))
+          .resolvers(classOf[Resolver2].getName)
           .load()
 
       flyway2.migrate()
